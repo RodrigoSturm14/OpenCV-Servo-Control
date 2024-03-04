71afaab0ca3e0cda71ea97596272a5aa2b07ebb4
Rodrigo Sturm <rodristurm@gmail.com>
2024-03-04 17:58:47 -0300

    Update: Draw the contours of the white areas from mask

--- color-detection/color-detection.py
+++ color-detection/color-detection.py
@@ -16,8 +16,12 @@ while ret:
   hsvFrame = cv2.cvtColor(frameF, cv2.COLOR_BGR2HSV)
   # crear mascara
   mask = cv2.inRange(hsvFrame, lowerLimit, upperLimit)
+  # extraer contornos (puntos limites de las areas blancas) de la mascara --> la imagen q 
+  # se usa tiene q estar en binario
+  contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cv2.drawContours(frameF, contours, -1, (0, 255, 0), 3)
 
-  cv2.imshow('camara', mask)
+  cv2.imshow('camara', frameF)
   if cv2.waitKey(40) & 0xFF == ord('q'):
     break
 
