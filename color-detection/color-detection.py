4d29ba4e46d608b46fbd9b52bd75ae9748e1ad6f
Rodrigo Sturm <rodristurm@gmail.com>
2024-03-05 18:13:41 -0300

    Update: Send msg throught serial port

--- color-detection/color-detection.py
+++ color-detection/color-detection.py
@@ -3,9 +3,12 @@ import numpy as np
 import serial
 from getHSVcolor import get_limits
 
-# ser = serial.Serial(
-#   port=0
-# )
+# COM5 es correcto; CERRAR ARDUINO SERIAL MONITOR Y CONECTAR ESP32 ANTES DE INICIAR PROGRAMA --> VA A TIRAR 
+# ERROR DE PERMISOS 
+ser = serial.Serial(
+  port= "COM5",
+  baudrate= 115200
+)
 
 # BGR_COLOR = [0, 255, 0]
 # lowerLimit, upperLimit = get_limits(BGR_COLOR)
@@ -13,7 +16,6 @@ from getHSVcolor import get_limits
 # limites color verde en hsv
 lowerLimit = np.array([36, 50, 70])
 upperLimit = np.array([89, 255, 255])
-
 
 # leer camara
 webcam = cv2.VideoCapture(0)
@@ -48,14 +50,17 @@ while ret:
       if(0 < x < 214):
         position = 'Left'
         print(position)
+        #ser.write(b'left\n')
       
       elif(214 < x < 428):
         position = 'Center'
         print(position)
+        ser.write(b'center\n')
       
       elif(428 < x < 640):
         position = 'Right'
         print(position)
+        #ser.write(b'right\n')
       
       # marcar centro
       cv2.circle(frameF, (x, y), 8, (183, 183, 22), -1)
@@ -67,10 +72,9 @@ while ret:
       cv2.putText(frameF, 'x: {}; y: {}'.format(x, y), (x + 15, y + 5), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 144, 255), 3)
       cv2.putText(frameF, '{}'.format(position), (x + 40, y + 40), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255), 3)
 
-
   cv2.imshow('camara', frameF)
   if cv2.waitKey(40) & 0xFF == ord('q'):
-    print(frameF.shape)
+    ser.close()
     break
 
 webcam.release()
