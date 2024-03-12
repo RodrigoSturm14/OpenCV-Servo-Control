d7e7164de231068c3d50642d4f4455e8cafd33ad
Rodrigo Sturm <rodristurm@gmail.com>
2024-03-12 13:33:46 -0300

    Update: Arduino nano able to move servo motor; 0, 90 and 180 degrees

--- micro-controller/servo-control-esp32/servo-control-esp32.ino
+++ micro-controller/servo-control-esp32/servo-control-esp32.ino
@@ -1,12 +1,19 @@
-#define LED_BUILTIN 2
+#include <Servo.h>
+
+#define LED_BUILTIN 13
+#define PIN_SERVO 9
 #define TICK_LED 1500
+
+Servo servoMotor;
+
 int newMillis = 0;
 String incomingString;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("iniciando");
   pinMode(LED_BUILTIN, OUTPUT);
+  servoMotor.attach(PIN_SERVO);
 }
 
 void loop() {
@@ -20,16 +27,19 @@ void loop() {
   // en el final de incomingString no tiene \n
   if(incomingString == "center"){ 
     digitalWrite(LED_BUILTIN, HIGH);
+    servoMotor.write(90);
     Serial.print("Moviendo al Centro");
   }
 
   else if(incomingString == "right"){
     digitalWrite(LED_BUILTIN, LOW);
+    servoMotor.write(0);
     Serial.print("Moviendo a la derecha");
   }
 
   else if(incomingString == "left"){
     digitalWrite(LED_BUILTIN, LOW);
+    servoMotor.write(180);
     Serial.print("Moviendo a la izquierda");
   }
 
